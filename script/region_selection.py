9afbab275d9f4b2e89aae0d6134fb947f8a74d2e
putintostyle <jackie84061258@gmail.com>
2021-08-07 16:18:18 +0800

    script

---
+++ script/region_selection.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import subprocess
+import time
+try:
+    import pydicom
+except:
+    subprocess.run(['pip', 'install',  'pydicom'])
+    import pydicom
+
+PATH = "D:\\下載\\gLymph test-20200429T121458Z-001\\gLymph test\\S5010 T2\\"
+
+def tellme(s):
+    print(s)
+    plt.title(s, fontsize=16)
+    plt.draw()
+
+##################################################
+# Define a triangle by clicking three points
+
+img = pydicom.read_file(PATH+'I70').pixel_array
+plt.clf()
+plt.imshow(img, cmap=plt.cm.bone)
+plt.gcf().canvas.draw() 
+plt.setp(plt.gca(), autoscale_on=False)
+
+tellme('Press enter to start')
+motion = plt.waitforbuttonpress()
+if motion == True:
+    tellme("double click to label")
+    # a = plt.ginput(1, timeout=-1, show_clicks=True)
+    
+    pts = []
+    # click_points = 
+    # print(click_points)
+    # pts.append(np.asarray(plt.ginput(1, timeout=-1, show_clicks=True))[0])
+    # print(np.array(pts)[:,0], np.array(pts)[:,1])
+    # plt.scatter(np.array(pts)[-1,0], np.array(pts)[-1,1])
+    while (len(pts)>=0)&(plt.waitforbuttonpress() != True):
+        # click_points = ()
+        pts.append(np.asarray(plt.ginput(1, timeout=-1,  show_clicks=True, mouse_add = 1))[0])
+        # print(pts)
+        plt.scatter(np.array(pts)[-1,0], np.array(pts)[-1,1])
+        plt.gcf().canvas.draw() 
+        if len(pts)>=2: 
+            plt.plot(np.array(pts)[-2:, 0] ,np.array(pts)[-2:, 1],  'r', lw=2)
+            plt.gcf().canvas.draw()
+        tellme("double space to leave double click to label")
+        if ((len(pts)>=2) & (pts[0] == pts[-1]).all() ):
+            plt.close()
+            break
+else:
+    tellme('Please tab to begin')
+    tellme('tab to begin')
+
+fig, ax = plt.subplots()
+ax.imshow(img, cmap=plt.cm.bone)
+lengthOfpts = len(pts)
+for pt_idx in range(lengthOfpts):
+    if pt_idx+1 < lengthOfpts:
+        ax.plot([pts[pt_idx][0], pts[pt_idx+1][0]], [pts[pt_idx][1], pts[pt_idx+1][1]], 'r', lw=2)
+    else:
+        ax.plot([pts[pt_idx][0], pts[0][0]], [pts[pt_idx][1], pts[0][1]], 'r', lw=2)
+plt.show()
