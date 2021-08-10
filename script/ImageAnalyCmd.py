a7714b0c37382cd7524c9207ce70a4c1b360ae74
putintostyle <jackie84061258@gmail.com>
2021-08-10 23:33:48 +0800

    test

--- script/ImageAnalyCmd.py
+++ script/ImageAnalyCmd.py
@@ -9,4 +9,10 @@ class ImageAnalyzerShell(ImageAnalyzerShellBase, analyzer):
     def __init__(self):
         ImageAnalyzerShellBase.__init__(self)
         self.analyzer = analyzer
-    
+    def do_select(self, args):
+        cmds = args.split()
+        slice = cmds[0]
+        label = cmds[1]
+        self.analyzer.select(slice, label) # 新增region上去
+    def do_regionshow(self, args): 
+        slef.analyzer.regionshow(slice)
