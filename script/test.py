21d404fbd0fd234b71896c5502b55db855b0ac76
putintostyle <jackie84061258@gmail.com>
2021-08-27 10:57:13 +0800

    on shell

---
+++ script/test.py
@@ -0,0 +1,21 @@
+class test:
+    def __init__ (self, tmp):
+        self.age = None
+        self.input = None
+    def next(a, b):
+        print(a+b)
+class add:
+    def __init__ (self):
+        pass
+
+    def a_year (self):
+        self.add = 3
+        print(self.input+self.add)
+
+class main(test):
+    def __init__ (self):
+        super().__init__()
+        self.age = 10
+        self.input = 10
+        self.next()
+test.next(10, 5)
